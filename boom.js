89f4c5ddd924f0f876be063067b69c49a9a89139
Raj Paul <raj@boostsecurity.io>
2021-03-08 09:36:31 -0500

    Commit again

--- boom.js
+++ boom.js
@@ -1,6 +1,7 @@
 
 				// we want the content to have some randomness to be able to push continuously
-				var version1 = 0.46587716124629863
+				var version2 = 0.811953977962466
 		
 				eval(getEvilCodeSomewhere() + Math.random())
+				eval(getSomeOtherEvilCode() + Math.random())
 			
