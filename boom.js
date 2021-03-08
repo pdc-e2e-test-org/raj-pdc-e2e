2ee083bc4196a09821d05648eae3df4a579ff07c
Raj Paul <raj@boostsecurity.io>
2021-03-08 09:36:17 -0500

    Commit two vulerabilities

---
+++ boom.js
@@ -0,0 +1,6 @@
+
+				// we want the content to have some randomness to be able to push continuously
+				var version1 = 0.46587716124629863
+		
+				eval(getEvilCodeSomewhere() + Math.random())
+			
