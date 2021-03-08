a52fb62e19f4b91e99e99dfe54e96e8a72585dbb
Raj Paul <raj@boostsecurity.io>
2021-03-08 09:32:37 -0500

    Add S3 bucket

---
+++ goat.tf
@@ -0,0 +1,10 @@
+
+# Goat.tf version 0.5532338119057574
+resource "aws_s3_bucket" "flowbucket" {
+bucket        = "${local.resource_prefix.value}-flowlogs"
+force_destroy = true
+tags = {
+Name        = "${local.resource_prefix.value}-flowlogs"
+Environment = local.resource_prefix.value
+}
+}
